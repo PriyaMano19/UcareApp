43c96f4e4f9cd455ca3257ba06bb8b79e9a39ef1
User <IT20224134@my.sliit.lk>
2021-09-25 15:58:47 +0530

    location java

---
+++ app/src/main/java/com/example/ucareapp/location.java
@@ -0,0 +1,39 @@
+package com.example.ucareapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.jetbrains.annotations.NotNull;
+
+public class location extends FragmentActivity implements OnMapReadyCallback {
+    GoogleMap mapAPI;
+    SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+        mapFragment= (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+        mapAPI=googleMap;
+        LatLng colombo=new LatLng(6.928381, 79.843267);
+        mapAPI.addMarker(new MarkerOptions().position(colombo).title("Colombo"));
+        mapAPI.moveCamera(CameraUpdateFactory.newLatLng(colombo));
+
+    }
+}
